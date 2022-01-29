afcdebea9728df4689f7678239b652e8271a2a82
Jean-François Senécal <jeanjeansenecal@gmail.com>
2022-01-29 14:15:23 -0500

    update search

--- PatientList.js
+++ PatientList.js
@@ -1,34 +1,27 @@
-
-
-const patients = []
-
-for (let i = 0; i < 100; i++) { 
-
+const patients = [];
+for (let i = 0; i < 100; i++) {
     var faker = require('faker');
 
     var randomName = faker.name.findName();
     var randombirth = faker.date.past();
     //var randomSex = faker.name.findName();
-    var randomAddress = faker.address.streetAddress(); 
-    var randomCity = faker.address.city(); 
-    var randomState = faker.address.state(); 
-    var randomCountry = faker.address.country(); 
-    var randombirth = faker.date.past(); 
+    var randomAddress = faker.address.streetAddress();
+    var randomCity = faker.address.city();
+    var randomState = faker.address.state();
+    var randomCountry = faker.address.country();
     var randombirth = faker.date.past();
-  
-let patient = 
+    var randombirth = faker.date.past();
 
+    let patient = {
+        name: `${randomName}`,
+        birth: `${randombirth}`,
+        Sex: 'Male',
+        streetAddress: `${randomAddress}`,
+        city: `${randomCity}`,
+        state: `${randomState}`,
+        country: `${randomCountry}`,
+    };
 
-{
-    "name" : randomName,
-    "birth": randombirth,
-    "Sex": "Male",
-    "streetAddress": randomAddress,
-    "city": randomCity,
-    "state": randomState,
-    "country": randomCountry
-};
-
-patients.push(patient);
+    patients.push(patient);
 }
-console.log(patients);
+export { patients };
--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,8 +1,10 @@
 import { StyleSheet } from 'react-native';
-import { useState, SetStateAction } from 'react';
+import React, { useState, SetStateAction } from 'react';
 import { Searchbar } from 'react-native-paper';
 
 import { Text, View } from '../components/Themed';
+
+import { patients } from '../PatientList';
 
 export default function SearchScreen({ navigation }: any) {
     const [searchQuery, setSearchQuery] = useState('');
@@ -10,15 +12,33 @@ export default function SearchScreen({ navigation }: any) {
 
     const onChangeSearch = (query: SetStateAction<string>) =>
         setSearchQuery(query);
+    console.log(patients);
+
+    const ResultCard = (item: any) => {
+        return <Text> {item}</Text>;
+    };
 
     return (
         <View style={styles.container}>
-            <Searchbar
-                placeholder="Search patient"
-                onChangeText={onChangeSearch}
-                value={searchQuery}
-                autoComplete
-            />
+            <View style={styles.container}>
+                <Searchbar
+                    placeholder="Search patient"
+                    onChangeText={onChangeSearch}
+                    value={searchQuery}
+                    autoComplete
+                />
+            </View>
+            {listOfProfiles.length === 0 ? (
+                <View>
+                    {listOfProfiles.map((item, id) => {
+                        <ResultCard key={id} item={item} />;
+                    })}
+                </View>
+            ) : (
+                <View style={styles.textBox}>
+                    <Text> No accounts found</Text>
+                </View>
+            )}
         </View>
     );
 }
@@ -33,5 +53,9 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         fontWeight: 'bold',
+    },
+    textBox: {
+        flex: 1,
+        alignItems: 'center',
     },
 });
