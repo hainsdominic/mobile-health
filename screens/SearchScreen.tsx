0458bc5f6e24c097638f2426611e20cca877b479
Jean-François Senécal <jeanjeansenecal@gmail.com>
2022-01-29 11:13:35 -0500

    added search bar

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,11 +1,24 @@
 import { StyleSheet } from 'react-native';
+import { useState, SetStateAction } from 'react';
+import { Searchbar } from 'react-native-paper';
 
 import { Text, View } from '../components/Themed';
 
 export default function SearchScreen({ navigation }: any) {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [listOfProfiles, setListOfProfiles] = useState([]);
+
+    const onChangeSearch = (query: SetStateAction<string>) =>
+        setSearchQuery(query);
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Search</Text>
+            <Searchbar
+                placeholder="Search patient"
+                onChangeText={onChangeSearch}
+                value={searchQuery}
+                autoComplete
+            />
         </View>
     );
 }
@@ -14,7 +27,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        padding: 10,
+        // justifyContent: 'center',
     },
     title: {
         fontSize: 20,
