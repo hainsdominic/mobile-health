b8119f18a5495e9369222458e4378dae52f94fe3
Stefan Cotargasanu <stefan.cotargasanu@polymtl.ca>
2022-01-29 14:50:13 -0500

    Added birthdate picker without styling

--- screens/NewPatientScreen.tsx
+++ screens/NewPatientScreen.tsx
@@ -1,27 +1,78 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { Button, Subheading, TextInput } from 'react-native-paper';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 import { View } from '../components/Themed';
+import { RadioButton } from 'react-native-paper/lib/typescript/components/RadioButton/RadioButton';
 
 export default function NewPatient({ navigation }: any) {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [birthdate, setBirthdate] = useState("");
-    const [sex, setSex] = useState("");
-    const [streetAddress, setStreetAddress] = useState("");
-    const [city, setCity] = useState("");
-    const [country, setCountry] = useState("");
-    
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [birthdate, setBirthdate] = useState(new Date());
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [sexAtBirth, setSexAtBirth] = useState('');
+    const [streetAddress, setStreetAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [country, setCountry] = useState('');
+
+    const onDateChange = (event: Event, date: Date) => {
+        const currentDate = date || birthdate;
+        setBirthdate(currentDate);
+    };
+
     return (
         <View style={styles.container}>
-            <TextInput autoComplete={false} placeholder='First Name' value={firstName} onChangeText={firstName => setFirstName(firstName)} style={styles.formElement} />
-            <TextInput autoComplete={false} placeholder='Last Name' value={lastName} onChangeText={lastName => setLastName(lastName)} style={styles.formElement} />
-            <TextInput autoComplete={false} placeholder='Birthdate' value={birthdate} onChangeText={birthdate => setBirthdate(birthdate)} style={styles.formElement} />
-            <TextInput autoComplete={false} placeholder='Sex' value={firstName} onChangeText={firstName => setFirstName(firstName)} style={styles.formElement} />
-            <TextInput autoComplete={false} placeholder='Street Address' value={firstName} onChangeText={firstName => setFirstName(firstName)} style={styles.formElement} />
-            <TextInput autoComplete={false} placeholder='City' value={firstName} onChangeText={firstName => setFirstName(firstName)} style={styles.formElement} />
-            <TextInput autoComplete={false} placeholder='Country' value={firstName} onChangeText={firstName => setFirstName(firstName)} style={styles.formElement} />
+            <TextInput
+                autoComplete={false}
+                placeholder="First Name"
+                value={firstName}
+                onChangeText={(firstName) => setFirstName(firstName)}
+                style={styles.formElement}
+            />
+            <TextInput
+                autoComplete={false}
+                placeholder="Last Name"
+                value={lastName}
+                onChangeText={(lastName) => setLastName(lastName)}
+                style={styles.formElement}
+            />
+            <Subheading>Birthdate</Subheading>
+            <DateTimePicker
+                value={birthdate}
+                mode="date"
+                onChange={onDateChange}
+                display="default"
+                style={styles.formElement}
+            />
+            <TextInput
+                autoComplete={false}
+                placeholder="Sex"
+                value={firstName}
+                onChangeText={(firstName) => setFirstName(firstName)}
+                style={styles.formElement}
+            />
+            <TextInput
+                autoComplete={false}
+                placeholder="Street Address"
+                value={firstName}
+                onChangeText={(firstName) => setFirstName(firstName)}
+                style={styles.formElement}
+            />
+            <TextInput
+                autoComplete={false}
+                placeholder="City"
+                value={firstName}
+                onChangeText={(firstName) => setFirstName(firstName)}
+                style={styles.formElement}
+            />
+            <TextInput
+                autoComplete={false}
+                placeholder="Country"
+                value={firstName}
+                onChangeText={(firstName) => setFirstName(firstName)}
+                style={styles.formElement}
+            />
         </View>
     );
 }
@@ -38,7 +89,6 @@ const styles = StyleSheet.create({
     },
     formElement: {
         marginVertical: 10,
-        width: 100 
-    }
-
+        width: 200,
+    },
 });
